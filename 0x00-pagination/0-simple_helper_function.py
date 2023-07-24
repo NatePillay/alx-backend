1e0e42db64621d212e1a85131889250c12284a5a
NatePillay <nathan94pillay@gmail.com>
2023-07-24 20:59:34 +0000

    new commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 '''chapter 1 getting to know the layput'''
 
-def index_range(page, page_size):
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+from typing import Tuple
 
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """ The function """
+    end_index: int = page * page_size
+    start_index: int = end_index - page_size
     return start_index, end_index
