7c05adabeb1315a909682482ff03f28e294e3e82
NatePillay <nathan94pillay@gmail.com>
2023-07-25 21:25:03 +0000

    new commit

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 '''0-main task'''
 BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    '''function basic cache inherits from base'''
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        '''assign item value for given key in cache'''
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if not key or not self.cache_data.get(key):
+            return None
+        return self.cache_data.get(key)
