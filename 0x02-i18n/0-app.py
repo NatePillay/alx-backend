a8f814a1a5daace2a78958077c640a116ba0ffec
NatePillay <nathan94pillay@gmail.com>
2023-08-02 03:45:01 +0000

    new commit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-
-from flask import Flask, render_template
+"""a script that starts a flask application"""
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello():
+    ''' returns a simple page'''
     return render_template('0-index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
