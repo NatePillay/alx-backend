e75abd5c350615f3ec5e749c75c07c4cecbbf578
NatePillay <nathan94pillay@gmail.com>
2023-09-19 20:18:04 +0000

    new commit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,14 @@
 """a script that starts a flask application"""
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'], stri)
 def hello():
     ''' returns a simple page'''
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
