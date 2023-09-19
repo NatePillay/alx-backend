b5d5550b1a7cff0f12be500be2305420b22c4429
NatePillay <nathan94pillay@gmail.com>
2023-09-19 20:07:59 +0000

    new commit

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
+"""A simple flask app
+"""
+
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-
-babel = Babel(app)
 
 class Config(object):
+    """ summary 
+    Returns: xxx
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
-app.config.from_object('1-app.Config')
 
-@app.route('/', method=['GET'], strict_slashes=False)
-def hello() -> str:
+@app.route('/')
+def index
     '''returns a simple page'''
     return render_template('1-index.html')
 
