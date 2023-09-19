b81f2d555ada050c41e52bd0b7749ef06e5085c0
NatePillay <nathan94pillay@gmail.com>
2023-09-19 20:16:50 +0000

    new commit

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,12 +8,13 @@ from flask_babel import Babel
 
 
 class Config(object):
-    """ summary 
+    """ summary
     Returns: xxx
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
